2b2afb0cac4f9995bdf54d4d93239d3556d72050
yangqun <yangqunxinli@163.com>
2025-07-24 10:45:28 +0800

    add run.sh

---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# this script must run outside containder.
+
+bin_path=$(dirname "$(readlink -f "$0")")
+sif_file="${bin_path}/sifs/packages.sif"
+
+if [ $# -ne 3 ]
+then
+    echo "Usage: $0 "samples_info.txt" "out_dir" "data_path"
+    exit 1
+fi
+
+singularity exec \
+    -e \
+    -b "$3" \
+    ${sif_file} \
+    snakemake \
+    --cores 3 \
+    -p \
+    --config info="$1" out_gz="True" out_dir="$2"
