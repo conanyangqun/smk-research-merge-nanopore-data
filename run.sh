a1c01deef2384b5867a9a182b81bd171d0c1dce3
yangqun <yangqunxinli@163.com>
2025-07-24 12:51:42 +0800

    fix some bugs in run.sh

--- run.sh
+++ run.sh
@@ -6,17 +6,17 @@ sif_file="${bin_path}/sifs/packages.sif"
 
 if [ $# -ne 3 ]
 then
-    echo "Usage: $0 "samples_info.txt" "out_dir" "data_path"
+    echo "Usage: $0 <samples_info.txt> <out_dir> <data_path>"
     exit 1
 fi
 
 singularity exec \
     -e \
-    -b "${bin_path}" \
-    -b "$3" \
+    -B "${bin_path}" \
+    -B "$3" \
     ${sif_file} \
     snakemake \
     --cores 3 \
     -p \
-    -s "{bin_path}/Snakefile" \
+    -s "${bin_path}/Snakefile" \
     --config info="$1" out_gz="True" out_dir="$2"
